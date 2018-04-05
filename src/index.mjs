9ff2d53848347a6dec5c9603205e0c1d82940d2c
Joe Fleming <joe.fleming@gmail.com>
2018-04-04 18:44:32 -0700

    chore: refactor into factory
    preferrable to newing up an instance since we can properly hide sauce details and internal state

--- src/index.mjs
+++ src/index.mjs
@@ -5,86 +5,107 @@ import openConnection from './open_connection.mjs';
 
 const randNum = len => parseInt(String(Math.random()).substr(2, len), 10);
 
-export default class HorseySauce extends events.EventEmitter {
-  constructor(sauceUser, sauceKey) {
-    super();
+export default function createRunner(sauceUser, sauceKey) {
+  const emitter = new events.EventEmitter();
+  const state = {
+    isRunning: false,
+    tunnel: null,
+    tunnelId: `horsey-sauce-${Date.now()}-${randNum(4)}-${randNum(6)}`,
+  };
 
-    this.sauceUser = sauceUser;
-    this.sauceKey = sauceKey;
-    this.isRunning = false;
-    this.tunnel = null;
-    this.tunnelId = `horsey-sauce-${Date.now()}-${randNum(4)}-${randNum(6)}`;
-  }
+  const onError = err => {
+    state.isRunning = false;
+    throw err;
+  };
 
-  // for capabilities, see https://saucelabs.com/docs/additional-config
-  run(src, runner, capabilities = {}) {
-    // only allow one running process at a time
-    if (this.isRunning) return Promise.reject(new Error('Another test is currently running'));
-
-    this.isRunning = true;
-
-    // create the sauce connect tunnel
-    this.emit('tunnel-connect');
-    return Promise.resolve(
-      this.tunnel || getTunnel(this.sauceUser, this.sauceKey, this.tunnelId)
-    ).then(
+  const getStateTunnel = () =>
+    Promise.resolve(state.tunnel || getTunnel(sauceUser, sauceKey, state.tunnelId)).then(
       tunnel => {
-        this.tunnel = tunnel;
-        this.emit('tunnel-connected');
+        if (!state.tunnel) emitter.emit('tunnel-ready', state.tunnelId);
+        state.tunnel = tunnel;
+      },
+      err => onError(err, this)
+    );
 
-        return getBrowser(this.sauceUser, this.sauceKey, capabilities, this.tunnelId).then(
+  return {
+    run(src, runner, capabilities = {}) {
+      // only allow one running process at a time
+      if (state.isRunning) return Promise.reject(new Error('Another test is currently running'));
+
+      state.isRunning = true;
+
+      // create the sauce connect tunnel
+      return getStateTunnel()
+        .then(() => getBrowser(sauceUser, sauceKey, capabilities, state.tunnelId))
+        .then(
           browser => {
-            this.emit('browser-ready');
+            emitter.emit('browser-ready');
 
             // do things in the browser
-            return openConnection(src, browser).then(
-              closeConnection =>
-                new Promise((resolve, reject) => {
-                  this.emit('runner-start');
-                  runner(browser, (err, data) => {
-                    this.isRunning = false;
-                    closeConnection().then(() => {
-                      if (err) {
-                        this.emit('runner-error', err);
-                        reject(err);
-                      } else {
-                        this.emit('runner-complete', data);
-                        resolve(data);
-                      }
-                    });
-                  });
-                }),
-              err => {
-                this.isRunning = false;
-                this.emit('connection-error', err);
-                throw err;
-              }
-            );
+            return openConnection(src, browser).then(closeConnection => ({
+              closeConnection,
+              browser,
+            }));
           },
-          err => {
-            this.isRunning = false;
-            this.emit('browser-error', err);
-            throw err;
-          }
-        );
-      },
-      err => {
-        this.isRunning = false;
-        this.emit('tunnel-error', err);
-        throw err;
-      }
-    );
-  }
+          err => onError(err, this)
+        )
+        .then(
+          ({ closeConnection, browser }) => {
+            emitter.emit('runner-start');
 
-  close() {
-    if (this.isRunning) return Promise.reject(new Error('Tests are still running'));
-    if (!this.tunnel) return Promise.reject(new Error('No tunner created'));
+            return new Promise((resolve, reject) => {
+              const cb = (err, data) => {
+                state.isRunning = false;
+                closeConnection().then(() => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    emitter.emit('runner-end', data);
+                    resolve(data);
+                  }
+                });
+              };
 
-    return new Promise(resolve =>
-      this.tunnel.close(() => {
-        this.emit('close');
-        resolve();
-      })
-    );
-  }
+              runner(browser, cb);
+            });
+          },
+          err => onError(err, this)
+        )
+        .catch(err => {
+          // final failsafe for any failures
+          state.running = false;
+          throw err;
+        });
+    },
+
+    on(name, handler) {
+      emitter.on(name, handler);
+      return () => emitter.removeListener(name, handler);
+    },
+
+    once(name, handler) {
+      emitter.once(name, handler);
+      return () => emitter.removeListener(name, handler);
+    },
+
+    close() {
+      if (state.isRunning) return Promise.reject(new Error('Tests are still running'));
+      if (!state.tunnel) return Promise.reject(new Error('No tunnel created'));
+
+      return new Promise(resolve =>
+        state.tunnel.close(() => {
+          state.tunnel = null;
+          state.isRunning = false;
+          emitter.emit('close');
+          resolve();
+        })
+      );
+    },
+
+    reset() {
+      return this.close().then(() => {
+        emitter.removeAllListeners();
+      });
+    },
+  };
 }
